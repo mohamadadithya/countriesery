08acc51089cf00584a395d946317e53394891184
mohamadadithya <mohamadadithya@gmail.com>
2022-03-25 14:17:31 +0700

    add number formatter for covid-19 stats

--- src/components/StatsCard.svelte
+++ src/components/StatsCard.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { numFormatter } from '../helpers/numFormatter';
+
 	export let covid;
 	let date = new Date(covid.updated).toLocaleDateString('en-EN', {
 		day: '2-digit',
@@ -7,23 +9,23 @@
 	});
 </script>
 
-<div class="p-8 shadow-custom rounded-md">
+<div class="p-6 shadow-custom rounded-md">
 	<h1 class="text-xl font-bold mb-6 border-b border-gray-200 pb-5">COVID-19 Stats</h1>
 	<ul class="flex justify-between items-center mb-10 border-b border-gray-200 pb-5 text-center">
 		<li>
 			<p class="font-bold">Confirmed</p>
 			<i class="text-5xl my-5 far fa-fw fa-frown text-yellow-500" />
-			<p class="text-xl">{covid.cases.toLocaleString()}</p>
+			<p class="text-xl">{numFormatter(covid.cases, 2)}</p>
 		</li>
 		<li>
 			<p class="font-bold">Recovered</p>
 			<i class="text-5xl my-5 far fa-fw fa-smile text-green-500" />
-			<p class="text-xl">{covid.recovered.toLocaleString()}</p>
+			<p class="text-xl">{numFormatter(covid.recovered, 2)}</p>
 		</li>
 		<li>
 			<p class="font-bold">Deaths</p>
 			<i class="text-5xl my-5 far fa-fw fa-dizzy text-red-500" />
-			<p class="text-xl">{covid.deaths.toLocaleString()}</p>
+			<p class="text-xl">{numFormatter(covid.deaths, 2)}</p>
 		</li>
 	</ul>
 	<p>
