925d66d42e570e387c1dca50565a96ff1d39fe3b
mohamadadithya <mohamadadithya@gmail.com>
2022-03-26 10:33:52 +0700

    add error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Something Wrong</title>
+</svelte:head>
+
+<div class="h-screen grid place-content-center text-center">
+	<h1 class="font-bold text-4xl mb-3">Something Wrong</h1>
+	<p>Country not found.</p>
+</div>
